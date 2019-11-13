789d1271e075167a878f0e348181b84644b375b4
DarkNoys <s-paradox@ro.ru>
2019-11-13 17:27:46 +0300

    Add news routes

---
+++ src/resourses/news/service.js
@@ -0,0 +1,21 @@
+const getNewsList = (userId) => {
+};
+
+const addNews = () => {
+};
+
+const getNews = (idNews) => {
+};
+
+const changeNews = (idNews) => {
+};
+
+const deleteNews = (idNews) => {
+};
+
+
+module.exports.getNewsList = getNewsList;
+module.exports.addNews = addNews;
+module.exports.getNews = getNews;
+module.exports.changeNews = changeNews;
+module.exports.deleteNews = deleteNews;
